dfa6c90d43300b3c9463c2fc5c3acb6c34f6e5aa
mgr <mgr@ffauth.com>
2018-02-26 19:54:31 +0100

    move file

---
+++ src/main/java/org/pryos/SimpleGuildBotForDiscord/Application/DiscordGuildConfiguration.java
@@ -0,0 +1,46 @@
+package org.pryos.SimpleGuildBotForDiscord.Application;
+
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
+import sx.blah.discord.handle.obj.IVoiceChannel;
+
+public class DiscordGuildConfiguration {
+
+	private IGuild guild;
+	private IChannel defaultTextChannel;
+	private IVoiceChannel defaultVoiceChannel;
+
+	public DiscordGuildConfiguration(IGuild oGuild) {
+		setGuild(oGuild);
+	}
+
+	public IGuild getGuild() {
+		return guild;
+	}
+
+	public void setGuild(IGuild oGuild) {
+		this.guild = oGuild;
+	}
+
+	public long getId() {
+		return getGuild().getLongID();
+	}
+
+	public IChannel getDefaultTextChannel() {
+		return defaultTextChannel;
+	}
+
+	public DiscordGuildConfiguration setDefaultTextChannel(IChannel defaultTextChannel) {
+		this.defaultTextChannel = defaultTextChannel;
+		return this;
+	}
+
+	public IVoiceChannel getDefaultVoiceChannel() {
+		return defaultVoiceChannel;
+	}
+
+	public DiscordGuildConfiguration setDefaultVoiceChannel(IVoiceChannel defaultVoiceChannel) {
+		this.defaultVoiceChannel = defaultVoiceChannel;
+		return this;
+	}
+}
